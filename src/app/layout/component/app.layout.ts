f85b62c0f91c3277b20018f747ccf4b4ed158b35
mehmet.karakoz <mehmetkarakoz0@gmail.com>
2025-04-09 22:05:33 +0300

    angular proje ilk commit

--- src/app/layout/component/app.layout.ts
+++ src/app/layout/component/app.layout.ts
@@ -14,14 +14,19 @@ import { LayoutService } from '../service/layout.service';
     template: `<div class="layout-wrapper" [ngClass]="containerClass">
         <app-topbar></app-topbar>
         <app-sidebar></app-sidebar>
+
         <div class="layout-main-container">
-            <div class="layout-main">
-                <router-outlet></router-outlet>
+            <div class="middleDiv">
+                <div class="layout-main">
+                    <router-outlet></router-outlet>
+                </div>
+                <app-footer></app-footer>
             </div>
-            <app-footer></app-footer>
         </div>
+
         <div class="layout-mask animate-fadein"></div>
-    </div> `
+    </div>
+    `
 })
 export class AppLayout {
     overlayMenuOpenSubscription: Subscription;
